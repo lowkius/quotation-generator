7ffa90ba06f046f50dbb11c9dcc09983c4ae647e
Tony H <tonyh0898@gmail.com>
2019-08-17 08:56:33 -0400

    Create quote-gen.js

---
+++ quote-gen.js
@@ -0,0 +1,23 @@
+var quotations = [
+  {
+  quote: `Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.`,
+  source: `Francis of Assisi`
+  {
+  quote: `I will love the light for it shows me the way, yet I will endure the darkeness because it shows me the stars.`,
+  source: `Og Mandino`
+  },
+  {
+  quote: `Your work is going to fill a large part of your life,and the only way to be truly satisfied is to do what you believe
+          is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking.
+          Don't settle. As with all matters of the heart, you'll know when you find it.`,
+  source: `Steve Jobs`
+  },
+  {
+  quote: `Happiness is not something you postpone for the future; it's something you design for the present.`,
+  source: `Jim Rohn`
+  },
+  {
+  quote: `I can't change the direction of the wind, but I can adjust my sails to always reach my destination.`,
+  source: `Jimmy Dean`
+  },
+]
